76abb09b2273833d8790919c17bd4b935fda23ec
sdsbzjh <sdzbzjh@gmail.com>
2018-07-17 09:52:45 +0800

    先怕去首页所有和分页的所有写真集，然后爬取每个写真集的图片

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const fs = require('fs')
 let imageCrawler = new Crawler({
     encoding: null,
     jQuery: false,
+    skipDuplicates: true,
     headers: {
         Referer: ''
     },
@@ -11,58 +12,108 @@ let imageCrawler = new Crawler({
         if (error) {
             console.log(error)
         } else {
-            const dirname = './images/' + res.options.path
+            const dirname = '/home/acer/图片/images/' + res.options.path
             const filename = res.options.filename
             fs.readdir(dirname, (err, files) => {
                 if (err) {
                     fs.mkdir(dirname, (error) => {
                         if (error) {
-                            console.log(error)
+                            console.error(error.Error)
                         } else {
-                            console.log('创建目录' + res.options.path + '成功')
+                            //console.log('创建目录' + res.options.path + '成功')
+                            fs.createWriteStream(dirname + '/' + filename).write(res.body)
                         }
                     })
                 } else {
-                    //imageCrawler
-                    //console.log(imageCrawler.options)
-                    // fs.writeFile(dirname + '/' + filename, '1', (err) => {
-                    //     if (err) {
-                    //         console.log(err);
-                    //     } else {
-
-                    //     }
-                    // })
+                    //保存图片
                     fs.createWriteStream(dirname + '/' + filename).write(res.body)
                 }
             })
         }
-        done();
+        done()
     }
 })
 
 
 let urlCrawler = new Crawler({
     maxConnections: 10,
+    skipDuplicates: true,
     callback(error, res, done) {
         if (error) {
             console.log(error)
         } else {
-            const $ = res.$;
+            const $ = res.$
             const title = $('h2.main-title').text()
             //目录名称，过滤掉页码
-            const path = title.split('（')[0].trim();
+            const path = title.split('（')[0].trim()
             const src = $('div.main-image img').attr('src')
+            if (!src) return
+
             const arr = src.split('.')
             const suffix = arr[arr.length - 1]
+            const uri = res.options.uri
+            //如果是第一页，那么爬取分页，否则不爬取分页
+            const reg = /www\.mzitu\.com\/[0-9]+$/
+            if (reg.test(uri)) {
+                const pages = $('.pagenavi>a')
+                const tmpArr = $(pages[pages.length - 2]).attr('href').split('/')
+                const lastPage = tmpArr[tmpArr.length - 1]
+                const urlPool = []
+
+                console.log(path + '---' + lastPage + '张已保存')
+
+                for (let i = 2; i < lastPage; ++i) {
+                    urlPool.push(uri + '/' + i)
+                }
+
+                urlCrawler.queue(urlPool)
+            }
+
             imageCrawler.options.headers.Referer = src
+            //启动下载的爬虫
             imageCrawler.queue({
                 url: src,
                 path: path,
                 filename: title + '.' + suffix
             })
         }
-        done();
+        done()
     }
 })
 
-urlCrawler.queue('http://www.mzitu.com/142189')
+//主爬虫程序
+let mainCrawler = new Crawler({
+    maxConnections: 10,
+    skipDuplicates: true,
+    callback(error, res, done) {
+        if (error) {
+            console.log(error)
+        } else {
+            const $ = res.$
+            const list = $('#pins>li>a')
+            const urls = []
+            for (let i = 0, len = list.length; i < len; ++i) {
+                urls.push($(list[i]).attr('href'))
+            }
+            urlCrawler.queue(urls)
+            const uri = res.options.uri
+            //如果是第一页，那么爬取分页，否则不爬取分页
+            const reg = /xinggan\/$/
+            if (reg.test(uri)) {
+                const pages = $('.nav-links>a')
+                const tmpArr = $(pages[pages.length - 2]).attr('href').split('/')
+                const lastPage = tmpArr[tmpArr.length - 2]
+                const urlPool = []
+
+                for (let i = 2; i < lastPage; ++i) {
+                    urlPool.push('http://www.mzitu.com/xinggan/page/' + i)
+                }
+
+                mainCrawler.queue(urlPool)
+            }
+        }
+        done()
+    }
+})
+
+mainCrawler.queue('http://www.mzitu.com/xinggan/')
